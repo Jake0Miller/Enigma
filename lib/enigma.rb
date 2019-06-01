772a32dfd47996c2a73dc3866c24b9fe094dc86d
Jake Miller <jake0miller@gmail.com>
2019-05-31 22:48:49 -0600

    Start cracking

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,10 @@
 require './lib/crypter'
 require './modules/date_maker'
+require './modules/cracker'
 
 class Enigma
   include DateMaker
+  include Cracker
 
   def encrypt(message, key = rand(99999).to_s.rjust(5,"0"), date = make_date)
     Crypter.crypt(message, key, date, 1)
@@ -10,5 +12,9 @@ class Enigma
 
   def decrypt(ciphertext, key, date = make_date)
     Crypter.crypt(ciphertext, key, date, -1)
+  end
+
+  def crack(ciphertext, date = make_date)
+    
   end
 end
