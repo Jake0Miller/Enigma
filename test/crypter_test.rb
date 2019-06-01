c812f593f5913a5b72f915ffe46672293b8f20f1
Jake Miller <jake0miller@gmail.com>
2019-05-31 22:18:04 -0600

    Finish encryption and decryption and file IO

--- test/crypter_test.rb
+++ test/crypter_test.rb
@@ -13,4 +13,11 @@ class CrypterTest < MiniTest::Test
   def test_shifter
     assert_equal [3, 27, 73, 20], @crypter.shifter("02715", "040895")
   end
+
+  def test_crypt
+    expected = {encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"}
+    assert_equal expected, Crypter.crypt("hello world", "02715", "040895", 1)
+  end
 end
