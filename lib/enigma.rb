1885fe7d405fdfdd9542f0b3446da7f8ef200c0f
Jake Miller <jake0miller@gmail.com>
2019-06-03 18:03:44 -0600

    Refactor a lot of things to handle edge cases

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,19 +5,21 @@ require './modules/date_formatter'
 class Enigma
   include DateFormatter
 
+  def initialize
+    @crypter = Crypter.new(("a".."z").to_a << " ")
+    @cracker = Cracker.new(("a".."z").to_a << " ")
+  end
+
   def encrypt(message, key = key_generator, date = make_date)
-    crypter = Crypter.new(("a".."z").to_a << " ")
-    crypter.encrypt(message, key, date)
+    @crypter.encrypt(message, key, date)
   end
 
   def decrypt(ciphertext, key, date = make_date)
-    crypter = Crypter.new(("a".."z").to_a << " ")
-    crypter.decrypt(ciphertext, key, date)
+    @crypter.decrypt(ciphertext, key, date)
   end
 
   def crack(ciphertext, date = make_date)
-    cracker = Cracker.new(("a".."z").to_a << " ")
-    cracker.crack(ciphertext, date)
+    @cracker.crack(ciphertext, date)
   end
 
   def key_generator
