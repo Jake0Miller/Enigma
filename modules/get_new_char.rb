4546b2d8acbd7bdf649a56ea9f3eb865387fa01f
Jake Miller <jake0miller@gmail.com>
2019-06-05 22:44:36 -0600

    Fix typo

--- modules/get_new_char.rb
+++ modules/get_new_char.rb
@@ -2,7 +2,7 @@ module GetNewChar
   def get_new_char(char, i, enc_or_decrypt = 0)
     if !ALPHABET.values.include?(char)
       char
-    else char != ' '
+    else
       ALPHABET[(REV[char] + enc_or_decrypt*i) % LENGTH]
     end
   end
